d947353ee47983d7dfbbdbc0a5bdbc209e44a31a
Akkaiah-pro <ita.paleru@gmail.com>
2025-06-17 21:27:04 +0530

    Update selenium.java

--- selenium.java
+++ selenium.java
@@ -1,1 +1,7 @@
 Welcome to Selenium Tutorials
+
+  Selenium is a free, open-source framework used to automate web applications. 
+  It enables testers and developers to write scripts that simulate user interactions with web pages, 
+  making it a valuable tool for testing and automating tasks.
+  
+  
